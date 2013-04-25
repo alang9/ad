9ab9ce1240363faf52bd5f88becb0932f8021d29
Alex Lang <me@alang.ca>
2013-04-25 16:27:09 +0900

    Remove still more things

--- src/Numeric/AD/Internal/Tower.hs
+++ src/Numeric/AD/Internal/Tower.hs
@@ -1,20 +1,6 @@
 {-# LANGUAGE TypeFamilies, FlexibleContexts #-}
-{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
--- {-# OPTIONS_HADDOCK hide, prune #-}
------------------------------------------------------------------------------
--- |
--- Module      : Numeric.AD.Tower.Internal
--- Copyright   : (c) Edward Kmett 2010
--- License     : BSD3
--- Maintainer  : ekmett@gmail.com
--- Stability   : experimental
--- Portability : GHC only
---
------------------------------------------------------------------------------
 
 module Numeric.AD.Internal.Tower () where
-
--- | @Tower@ is an AD 'Mode' that calculates a tangent tower by forward AD, and provides fast 'diffsUU', 'diffsUF'
 
 type family Scalar t
 
@@ -23,18 +9,11 @@ newtype Tower s a = Tower [a]
 type instance Scalar (Tower s a) = a
 
 class (Num (Scalar t), Num t) => Mode t where
-    auto  :: Scalar t -> t
     (<+>) :: t -> t -> t
 
 instance (Num a) => Mode (Tower s a) where
-    auto a = Tower [a]
-
-    Tower [] <+> bs = bs
-    as <+> Tower [] = as
-    Tower (a:as) <+> Tower (b:bs) = Tower (c:cs)
-        where
-            c = a + b
-            Tower cs = Tower as <+> Tower bs
-
+    Tower as <+> _ = undefined
+      where
+        _ = (Tower as) <+> (Tower as)
 
 instance Num a => Num (Tower s a) where
