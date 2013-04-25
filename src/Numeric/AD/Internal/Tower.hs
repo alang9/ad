c19fbbe32eb5d10702d15da4c9f1626ae7d8e52b
Alex Lang <me@alang.ca>
2013-04-25 16:13:12 +0900

    Remove more things...

--- src/Numeric/AD/Internal/Tower.hs
+++ src/Numeric/AD/Internal/Tower.hs
@@ -14,8 +14,6 @@
 
 module Numeric.AD.Internal.Tower () where
 
-import Control.Monad
-
 -- | @Tower@ is an AD 'Mode' that calculates a tangent tower by forward AD, and provides fast 'diffsUU', 'diffsUF'
 
 type family Scalar t
@@ -28,32 +26,6 @@ class (Num (Scalar t), Num t) => Mode t where
     auto  :: Scalar t -> t
     (<+>) :: t -> t -> t
 
-one :: Mode t => t
-one = auto 1
-{-# INLINE one #-}
-
-instance Show a => Show (Tower s a) where
-    showsPrec n (Tower as) = showParen (n > 10) $ showString "Tower " . showList as
-
--- Local combinators
-
-tangents :: Tower s a -> Tower s a
-tangents (Tower []) = Tower []
-tangents (Tower (_:xs)) = Tower xs
-{-# INLINE tangents #-}
-
-truncated :: Tower s a -> Bool
-truncated (Tower []) = True
-truncated _ = False
-{-# INLINE truncated #-}
-
-bundle :: a -> Tower s a -> Tower s a
-bundle a (Tower as) = Tower (a:as)
-{-# INLINE bundle #-}
-
-primal (Tower (x:_)) = x
-primal _ = 0
-
 instance (Num a) => Mode (Tower s a) where
     auto a = Tower [a]
 
@@ -64,58 +36,5 @@ instance (Num a) => Mode (Tower s a) where
             c = a + b
             Tower cs = Tower as <+> Tower bs
 
-lift1 f df b   = bundle (f (primal b)) (tangents b * df b)
-
-lift1_ f df b = a where
-    a = bundle (f (primal b)) (tangents b * df a b)
-
-binary f dadb dadc b c = bundle (f (primal b) (primal c)) (tangents b * dadb + tangents c * dadc)
-
-lift2 f df b c =
-  bundle (f (primal b) (primal c)) tana
-  where (dadb, dadc) = df b c
-        tanb = tangents b
-        tanc = tangents c
-        tana = case (truncated tanb, truncated tanc) of
-          (False, False) -> tanb * dadb + tanc * dadc
-          (True, False) -> tanc * dadc
-          (False, True) -> tanb * dadb
-          (True, True) -> Tower []
 
 instance Num a => Num (Tower s a) where
-    fromInteger 0  = Tower []
-    fromInteger n = auto (fromInteger n)
-    (+)          = (<+>) -- binary (+) one one
-    (-)          = binary (-) (auto 1) (auto (-1)) -- TODO: <-> ? as it is, this might be pretty bad for Tower
-    (*)          = lift2 (*) (\x y -> (y, x))
-    negate       = lift1 negate (const (auto (-1)))
-    abs          = lift1 abs signum
-    signum a     = lift1 signum (const $ Tower []) a
-instance Fractional a => Fractional (Tower s a) where
-    fromRational 0 = Tower []
-    fromRational r = auto (fromRational r)
-    x / y        = x * recip y
-    recip        = lift1_ recip (const . negate . join (*))
-instance Floating a => Floating (Tower s a) where
-    pi       = auto pi
-    exp      = lift1_ exp const
-    log      = lift1 log recip
-    logBase x y = log y / log x
-    sqrt     = lift1_ sqrt (\z _ -> recip (auto 2 * z))
-    (**)     = undefined
-    --x ** y
-    --   | isKnownZero y     = 1
-    --   | isKnownConstant y, y' <- primal y = lift1 (** y') ((y'*) . (**(y'-1))) x
-    --   | otherwise         = lift2_ (**) (\z xi yi -> (yi * z / xi, z * log1 xi)) x y
-    sin      = lift1 sin cos
-    cos      = lift1 cos $ negate . sin
-    tan      = lift1 tan $ recip . join (*) . cos
-    asin     = lift1 asin $ \x -> recip (sqrt (auto 1 - join (*) x))
-    acos     = lift1 acos $ \x -> negate (recip (sqrt (one - join (*) x)))
-    atan     = lift1 atan $ \x -> recip (one + join (*) x)
-    sinh     = lift1 sinh cosh
-    cosh     = lift1 cosh sinh
-    tanh     = lift1 tanh $ recip . join (*) . cosh
-    asinh    = lift1 asinh $ \x -> recip (sqrt (one + join (*) x))
-    acosh    = lift1 acosh $ \x -> recip (sqrt (join (*) x - one))
-    atanh    = lift1 atanh $ \x -> recip (one - join (*) x)
