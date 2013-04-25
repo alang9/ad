ec7320844110c9586fb2d44fa17fbaa4e60fa8b4
Alex Lang <me@alang.ca>
2013-04-25 16:02:43 +0900

    Shave off some more things

--- src/Numeric/AD/Internal/Classes.hs
+++ src/Numeric/AD/Internal/Classes.hs
@@ -19,18 +19,10 @@ module Numeric.AD.Internal.Classes
       Mode(..)
     , one
     -- * Automatically Deriving AD
-    , Jacobian(..)
     , Primal(..)
-    , deriveNumeric
     , Iso(..)
     , Scalar
     ) where
-
-import Control.Applicative ((<$>), pure)
-import Control.Monad
-import Data.Number.Erf
-import Data.Proxy
-import Language.Haskell.TH.Syntax
 
 type family Scalar t
 
@@ -42,63 +34,13 @@ instance Iso a a where
     iso = id
     osi = id
 
-infixr 6 <+>
-infixr 7 *^
-infixl 7 ^*
-infixr 7 ^/
-infixr 8 <**>
-
 class (Num (Scalar t), Num t) => Mode t where
-    -- | allowed to return False for items with a zero derivative, but we'll give more NaNs than strictly necessary
-    isKnownConstant :: t -> Bool
-    isKnownConstant _ = False
-
-    -- | allowed to return False for zero, but we give more NaN's than strictly necessary then
-    isKnownZero :: t -> Bool
-    isKnownZero _ = False
-
-    -- | Embed a constant
     auto  :: Scalar t -> t
-
-    -- | Vector sum
     (<+>) :: t -> t -> t
-
-    -- | Scalar-vector multiplication
-    (*^) :: Scalar t -> t -> t
-
-    -- | Vector-scalar multiplication
-    (^*) :: t -> Scalar t -> t
-
-    -- | Scalar division
-    (^/) :: (Num t, Fractional (Scalar t)) => t -> Scalar t -> t
-
-    -- | Exponentiation, this should be overloaded if you can figure out anything about what is constant!
-    (<**>) :: (Floating (Scalar t)) => t -> t -> t
-
-    -- default (<**>) :: (Jacobian t, Floating (D t), Floating (Scalar t)) => t -> t -> t
-    -- x <**> y = lift2_ (**) (\z xi yi -> (yi * z / xi, z * log xi)) x y
-
-    -- | > 'zero' = 'lift' 0
-    zero :: t
-
-#ifndef HLINT
-    default (*^) :: Num t => Scalar t -> t -> t
-    a *^ b = auto a * b
-    default (^*) :: Num t => t -> Scalar t -> t
-    a ^* b = a * auto b
-#endif
-
-    a ^/ b = a ^* recip b
-
-    zero = auto 0
 
 one :: Mode t => t
 one = auto 1
 {-# INLINE one #-}
-
-negOne :: Mode t => t
-negOne = auto (-1)
-{-# INLINE negOne #-}
 
 -- | 'Primal' is used by 'deriveMode' but is not exposed
 -- via the 'Mode' class to prevent its abuse by end users
@@ -111,96 +53,3 @@ negOne = auto (-1)
 
 class Primal t where
     primal :: t -> Scalar t
-
--- | 'Jacobian' is used by 'deriveMode' but is not exposed
--- via 'Mode' to prevent its abuse by end users
--- via the 'AD' data type.
-class (Mode t, Mode (D t), Num (D t)) => Jacobian t where
-    type D t :: *
-
-    unary  :: (Scalar t -> Scalar t) -> D t -> t -> t
-    lift1  :: (Scalar t -> Scalar t) -> (D t -> D t) -> t -> t
-    lift1_ :: (Scalar t -> Scalar t) -> (D t -> D t -> D t) -> t -> t
-
-    binary :: (Scalar t -> Scalar t -> Scalar t) -> D t -> D t -> t -> t -> t
-    lift2  :: (Scalar t -> Scalar t -> Scalar t) -> (D t -> D t -> (D t, D t)) -> t -> t -> t
-    lift2_ :: (Scalar t -> Scalar t -> Scalar t) -> (D t -> D t -> D t -> (D t, D t)) -> t -> t -> t
-
-withPrimal :: (Jacobian t, Scalar t ~ Scalar (D t)) => t -> Scalar t -> t
-withPrimal t a = unary (const a) one t
-{-# INLINE withPrimal #-}
-
-fromBy :: (Jacobian t, Scalar t ~ Scalar (D t)) => t -> t -> Int -> Scalar t -> t
-fromBy a delta n x = binary (\_ _ -> x) one (fromIntegral n) a delta
-
-discrete1 :: Primal t => (Scalar t -> c) -> t -> c
-discrete1 f x = f (primal x)
-{-# INLINE discrete1 #-}
-
-discrete2 :: Primal t => (Scalar t -> Scalar t -> c) -> t -> t -> c
-discrete2 f x y = f (primal x) (primal y)
-{-# INLINE discrete2 #-}
-
-discrete3 :: Primal t => (Scalar t -> Scalar t -> Scalar t -> d) -> t -> t -> t -> d
-discrete3 f x y z = f (primal x) (primal y) (primal z)
-{-# INLINE discrete3 #-}
-
--- | @'deriveNumeric g s@ provides the following instances:
---
--- > instance ('Num' a, 'Enum' a) => 'Enum' ($g a s)
--- > instance ('Num' a, 'Eq' a) => 'Eq' ($g a s)
--- > instance ('Num' a, 'Ord' a) => 'Ord' ($g a s)
--- > instance ('Num' a, 'Bounded' a) => 'Bounded' ($g a s)
---
--- > instance ('Num' a) => 'Num' ($g a s)
--- > instance ('Fractional' a) => 'Fractional' ($g a s)
--- > instance ('Floating' a) => 'Floating' ($g a s)
--- > instance ('RealFloat' a) => 'RealFloat' ($g a s)
--- > instance ('RealFrac' a) => 'RealFrac' ($g a s)
--- > instance ('Real' a) => 'Real' ($g a s)
-deriveNumeric :: ([Pred] -> [Pred]) -> Type -> Type -> Q [Dec]
-deriveNumeric f tCon s' = map fudgeCxt <$> lifted
-    where
-      t = pure tCon
-      -- s = pure s'
-      fudgeCxt (InstanceD cxt typ dec) = InstanceD (f cxt) typ dec
-      fudgeCxt _ = error "Numeric.AD.Internal.Classes.deriveNumeric_fudgeCxt: Not InstanceD"
-      lifted = [d|
-       instance Num a => Num ($t a) where
-        fromInteger 0  = zero
-        fromInteger n = auto (fromInteger n)
-        (+)          = (<+>) -- binary (+) one one
-        (-)          = binary (-) (auto 1) (auto (-1)) -- TODO: <-> ? as it is, this might be pretty bad for Tower
-        (*)          = lift2 (*) (\x y -> (y, x))
-        negate       = lift1 negate (const (auto (-1)))
-        abs          = lift1 abs signum
-        signum a     = lift1 signum (const zero) a
-       instance Fractional a => Fractional ($t a) where
-        fromRational 0 = zero
-        fromRational r = auto (fromRational r)
-        x / y        = x * recip y
-        recip        = lift1_ recip (const . negate . join (*))
-       instance Floating a => Floating ($t a) where
-        pi       = auto pi
-        exp      = lift1_ exp const
-        log      = lift1 log recip
-        logBase x y = log y / log x
-        sqrt     = lift1_ sqrt (\z _ -> recip (auto 2 * z))
-        (**)     = (<**>)
-        --x ** y
-        --   | isKnownZero y     = 1
-        --   | isKnownConstant y, y' <- primal y = lift1 (** y') ((y'*) . (**(y'-1))) x
-        --   | otherwise         = lift2_ (**) (\z xi yi -> (yi * z / xi, z * log1 xi)) x y
-        sin      = lift1 sin cos
-        cos      = lift1 cos $ negate . sin
-        tan      = lift1 tan $ recip . join (*) . cos
-        asin     = lift1 asin $ \x -> recip (sqrt (auto 1 - join (*) x))
-        acos     = lift1 acos $ \x -> negate (recip (sqrt (one - join (*) x)))
-        atan     = lift1 atan $ \x -> recip (one + join (*) x)
-        sinh     = lift1 sinh cosh
-        cosh     = lift1 cosh sinh
-        tanh     = lift1 tanh $ recip . join (*) . cosh
-        asinh    = lift1 asinh $ \x -> recip (sqrt (one + join (*) x))
-        acosh    = lift1 acosh $ \x -> recip (sqrt (join (*) x - one))
-        atanh    = lift1 atanh $ \x -> recip (one - join (*) x)
-       |]
--- src/Numeric/AD/Internal/Tower.hs
+++ src/Numeric/AD/Internal/Tower.hs
@@ -15,14 +15,12 @@
 module Numeric.AD.Internal.Tower
     () where
 
+import Control.Monad
 import Prelude hiding (all)
-import Data.Data (Data)
-import Data.Typeable (Typeable)
-import Language.Haskell.TH
 import Numeric.AD.Internal.Classes
 
 -- | @Tower@ is an AD 'Mode' that calculates a tangent tower by forward AD, and provides fast 'diffsUU', 'diffsUF'
-newtype Tower s a = Tower { getTower :: [a] } deriving (Data, Typeable)
+newtype Tower s a = Tower [a]
 
 type instance Scalar (Tower s a) = a
 
@@ -51,11 +49,6 @@ instance Num a => Primal (Tower s a) where
 
 instance (Num a) => Mode (Tower s a) where
     auto a = Tower [a]
-    zero = Tower []
-    Tower [] <**> y         = auto (0 ** primal y)
-    _        <**> Tower []  = auto 1
-    x        <**> Tower [y] = lift1 (**y) (\z -> y *^ z <**> Tower [y-1]) x
-    x        <**> y         = lift2_ (**) (\z xi yi -> (yi * z / xi, z * log xi)) x y
 
     Tower [] <+> bs = bs
     as <+> Tower [] = as
@@ -64,33 +57,58 @@ instance (Num a) => Mode (Tower s a) where
             c = a + b
             Tower cs = Tower as <+> Tower bs
 
-    a *^ Tower bs = Tower (map (a*) bs)
-    Tower as ^* b = Tower (map (*b) as)
-    Tower as ^/ b = Tower (map (/b) as)
+lift1 f df b   = bundle (f (primal b)) (tangents b * df b)
 
-instance Num a => Jacobian (Tower s a) where
-    type D (Tower s a) = Tower s a
-    unary f dadb b = bundle (f (primal b)) (tangents b * dadb)
-    lift1 f df b   = bundle (f (primal b)) (tangents b * df b)
-    lift1_ f df b = a where
-        a = bundle (f (primal b)) (tangents b * df a b)
+lift1_ f df b = a where
+    a = bundle (f (primal b)) (tangents b * df a b)
 
-    binary f dadb dadc b c = bundle (f (primal b) (primal c)) (tangents b * dadb + tangents c * dadc)
-    lift2 f df b c =
-      bundle (f (primal b) (primal c)) tana
-      where (dadb, dadc) = df b c
-            tanb = tangents b
-            tanc = tangents c
-            tana = case (truncated tanb, truncated tanc) of
-              (False, False) -> tanb * dadb + tanc * dadc
-              (True, False) -> tanc * dadc
-              (False, True) -> tanb * dadb
-              (True, True) -> zero
-    lift2_ f df b c = a where
-        a0 = f (primal b) (primal c)
-        da = tangents b * dadb + tangents c * dadc
-        a = bundle a0 da
-        (dadb, dadc) = df a b c
+binary f dadb dadc b c = bundle (f (primal b) (primal c)) (tangents b * dadb + tangents c * dadc)
 
-let s = VarT (mkName "s") in
-  deriveNumeric id (ConT ''Tower `AppT` s) s
+lift2 f df b c =
+  bundle (f (primal b) (primal c)) tana
+  where (dadb, dadc) = df b c
+        tanb = tangents b
+        tanc = tangents c
+        tana = case (truncated tanb, truncated tanc) of
+          (False, False) -> tanb * dadb + tanc * dadc
+          (True, False) -> tanc * dadc
+          (False, True) -> tanb * dadb
+          (True, True) -> Tower []
+
+instance Num a => Num (Tower s a) where
+    fromInteger 0  = Tower []
+    fromInteger n = auto (fromInteger n)
+    (+)          = (<+>) -- binary (+) one one
+    (-)          = binary (-) (auto 1) (auto (-1)) -- TODO: <-> ? as it is, this might be pretty bad for Tower
+    (*)          = lift2 (*) (\x y -> (y, x))
+    negate       = lift1 negate (const (auto (-1)))
+    abs          = lift1 abs signum
+    signum a     = lift1 signum (const $ Tower []) a
+instance Fractional a => Fractional (Tower s a) where
+    fromRational 0 = Tower []
+    fromRational r = auto (fromRational r)
+    x / y        = x * recip y
+    recip        = lift1_ recip (const . negate . join (*))
+instance Floating a => Floating (Tower s a) where
+    pi       = auto pi
+    exp      = lift1_ exp const
+    log      = lift1 log recip
+    logBase x y = log y / log x
+    sqrt     = lift1_ sqrt (\z _ -> recip (auto 2 * z))
+    (**)     = undefined
+    --x ** y
+    --   | isKnownZero y     = 1
+    --   | isKnownConstant y, y' <- primal y = lift1 (** y') ((y'*) . (**(y'-1))) x
+    --   | otherwise         = lift2_ (**) (\z xi yi -> (yi * z / xi, z * log1 xi)) x y
+    sin      = lift1 sin cos
+    cos      = lift1 cos $ negate . sin
+    tan      = lift1 tan $ recip . join (*) . cos
+    asin     = lift1 asin $ \x -> recip (sqrt (auto 1 - join (*) x))
+    acos     = lift1 acos $ \x -> negate (recip (sqrt (one - join (*) x)))
+    atan     = lift1 atan $ \x -> recip (one + join (*) x)
+    sinh     = lift1 sinh cosh
+    cosh     = lift1 cosh sinh
+    tanh     = lift1 tanh $ recip . join (*) . cosh
+    asinh    = lift1 asinh $ \x -> recip (sqrt (one + join (*) x))
+    acosh    = lift1 acosh $ \x -> recip (sqrt (join (*) x - one))
+    atanh    = lift1 atanh $ \x -> recip (one - join (*) x)
